bad83e895bf8f812f4d30fc57733594c91354ffa
陈婉君 <chenwanjun@meprint.com>
2021-12-24 17:01:10 +0800

    feat:viewMatrix modelMatrix faceToUp Projection prespective fix depth conflict draw cube

--- lib.js
+++ lib.js
@@ -1,6 +1,7 @@
 class Gl {
   constructor(canvas){
     this.gl = canvas.getContext("webgl");
+    this.canvas = canvas;
     this.texUnits = {};
 
     if(!this.gl){
@@ -147,7 +148,16 @@ class Gl {
       gl.vertexAttribPointer(location, length, gl.FLOAT, false, stride*fSize, offset*fSize);
       gl.enableVertexAttribArray(location)
     }
+
   }
+
+  setBufferIndex({ index }) {
+    const { gl } = this;
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, index, gl.STATIC_DRAW);
+  }
+
 
   drawPoint(count){
     const { gl } = this;
@@ -172,14 +182,37 @@ class Gl {
   }
 
 
-  drawTriangle(count){
+  drawTriangle(count,offset,noClear){
     const { gl } = this;
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // 用不透明黑色清空画布
+    offset = offset ? offset : 0;
+    if (!noClear) {
+      gl.clearColor(0.0, 0.0, 0.0, 1.0);  // 用不透明黑色清空画布
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    }
+    gl.enable(gl.DEPTH_TEST);//open depth test
+    gl.enable(gl.POLYGON_OFFSET_FILL);//open polygon offset
+    // gl.polygonOffset(1.0, 1.0);
     // 清除画布在绘制前
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    // gl.drawArrays(gl.TRIANGLES, 0, count);//三角形
+    gl.drawArrays(gl.TRIANGLES, offset, count);//三角形
     // gl.drawArrays(gl.TRIANGLE_STRIP, 0, count);//三角形带
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, count);//三角形扇
+    // gl.drawArrays(gl.TRIANGLE_FAN, 0, count);//三角形扇
+  }
+
+
+  drawTriangleByElements(count,offset,noClear){
+    const { gl } = this;
+    offset = offset ? offset : 0;
+    if (!noClear) {
+      gl.clearColor(0.0, 0.0, 0.0, 1.0);  // 用不透明黑色清空画布
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    }
+    gl.enable(gl.DEPTH_TEST);//open depth test
+    gl.enable(gl.POLYGON_OFFSET_FILL);//open polygon offset
+    // gl.polygonOffset(1.0, 1.0);
+    // 清除画布在绘制前
+    gl.drawElements(gl.TRIANGLES,count,gl.UNSIGNED_BYTE,0);//三角形
+    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, count);//三角形带
+    // gl.drawArrays(gl.TRIANGLE_FAN, 0, count);//三角形扇
   }
 
 
@@ -216,5 +249,74 @@ class Gl {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
     gl.uniform1i(uSampler, texUnit)
+  }
+
+  setViewMatrix({ eyes, center, normal }) {
+    const { gl, program } = this;
+    const key= 'uViewMatrix';
+    const viewMatrix = new Matrix4();
+    viewMatrix.setLookAt(eyes.x,eyes.y,eyes.z,center.x,center.y,center.z, normal.x,normal.y,normal.z);
+    const uViewMatrix = gl.getUniformLocation(program, key)
+    gl.uniformMatrix4fv(uViewMatrix,false,viewMatrix.elements);
+  }
+
+  setModelMatrix({ angle, axis }) {
+    const { gl, program } = this;
+    const key= 'uModelMatrix';
+    const modelMatrix = new Matrix4();
+    const x = axis === 'x' ? 1 : 0;
+    const y = axis === 'y' ? 1 : 0;
+    const z = axis === 'z' ? 1 : 0;
+    modelMatrix.setRotate(angle,x,y,z);
+    const uModelMatrix = gl.getUniformLocation(program, key)
+    gl.uniformMatrix4fv(uModelMatrix,false,modelMatrix.elements);
+  }
+
+  setOrtho({ left, right, bottom, top, near, far }) {
+    const { gl, program } = this;
+    const key= 'uProMatrix';
+    const proMatrix = new Matrix4();
+    left = left !== undefined ? left : -1.0;
+    right = right !== undefined ? right : 1.0;
+    bottom = bottom !== undefined ? bottom: -1.0;
+    top = top !== undefined ? top : 1.0;
+    near = near !== undefined ? near : 0.0;
+    far = far !== undefined ? far : 1.0;
+    proMatrix.setOrtho(left, right, bottom, top, near, far);
+    const uProMatrix = gl.getUniformLocation(program, key)
+    gl.uniformMatrix4fv(uProMatrix,false,proMatrix.elements);
+  }
+
+
+  setPerspective({ fovy, aspect, near, far }) {
+    const { gl, program } = this;
+    const key= 'uProMatrix';
+    const proMatrix = new Matrix4();
+    const canvas = this.canvas;
+    const { width, height } = canvas;
+    fovy = fovy !== undefined ? fovy : 1.0;
+    aspect = aspect !== undefined ? aspect: width / height;
+    near = near !== undefined ? near : 0.0;
+    far = far !== undefined ? far : 1.0;
+    proMatrix.setPerspective(fovy, aspect, near, far);
+    const uProMatrix = gl.getUniformLocation(program, key)
+    gl.uniformMatrix4fv(uProMatrix,false,proMatrix.elements);
+  }
+
+
+  setMVPMatrix({ fovy, aspect, near, far, eyes, center, normal}) {
+    const { gl, program } = this;
+    const canvas = this.canvas;
+    const { width, height } = canvas;
+    const uMVPMatrix = gl.getUniformLocation(program, 'uMVPMatrix');
+    fovy = fovy !== undefined ? fovy : 1.0;
+    aspect = aspect !== undefined ? aspect: width / height;
+    near = near !== undefined ? near : 0.0;
+    far = far !== undefined ? far : 1.0;
+    const MVPMatrix = new Matrix4();
+    MVPMatrix.setPerspective(fovy, aspect, near, far);
+    MVPMatrix.setLookAt(eyes.x, eyes.y, eyes.z, center.x, center.y, center.z, normal.x, normal.y, normal.z);
+    // console.log(MVPMatrix)
+    gl.uniformMatrix4fv(uMVPMatrix, false, MVPMatrix.elements);
   }
 }
